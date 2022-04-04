a3054d6ca3ea27428c22a66344ce20fc21cbe11e
M3lani03 <vasseurmelanie.pro@gmail.com>
2022-04-04 09:47:05 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
 module "network" {
   source  = "app.terraform.io/melanievasseur/network/azurerm"
   version = "3.5.0"
-  # insert required variables here
+
+  resource_group_name = "azurevm"
 }
